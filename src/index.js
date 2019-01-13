43c27f582a865ae53e57f82424d29bd03713f5b4
MauMau15 <mauriciorenero@gmail.com>
2019-01-12 22:30:25 -0600

    creating react components through functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-let formatName = name => {
-  return name.toUpperCase();
-} 
-const name = 'Mauricio Renero';
-const element = <div className="waves-effect waves-light btn">Hello {formatName(name)}</div>
+const Welcome = (props) => {
+  return <h1>Hello {props.name}</h1>
+}
+
+const element = <Welcome name="Mauricio Renero"></Welcome>
+
 ReactDOM.render(
   element,
   document.getElementById('root')
-);
+)
