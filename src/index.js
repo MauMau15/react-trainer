4a53d415953eb774dd6594342228adef221f34a7
MauMau15 <mauriciorenero@gmail.com>
2019-01-12 22:03:55 -0600

    Understanding JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let formatName = name => {
+  return name.toUpperCase();
+} 
+const name = 'Mauricio Renero';
+const element = <div className="waves-effect waves-light btn">Hello {formatName(name)}</div>
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
